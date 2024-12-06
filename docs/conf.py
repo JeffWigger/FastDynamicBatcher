e372b7c11e3faf75743955d611b927332168fe27
Jeffrey <wiggerjeffrey@gmail.com>
2024-12-06 21:56:47 +0100

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath("../"))
 project = "Fast Dynamic Batcher"
 copyright = "2024, Jeffrey Wigger"
 author = "Jeffrey Wigger"
-release = "0.1.1"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
